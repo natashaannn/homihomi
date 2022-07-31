047f47860088a370d5fcc37c469e706519720b22
natashaannn <natashaannlum@gmail.com>
2022-07-31 21:22:11 +0800

    Added long-lived access token expiring sep 29

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { Page, ApiResponse } from 'types/types'
+import { Page, ApiResponse, Post } from 'types/types'
+import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 import ListingCard from '@components/Listing/Listing'
 import NavBar from '@components/NavBar/NavBar'
 import type { NextPage } from 'next'
-import Grid from '@mui/material/Grid'
-import { useEffect, useState } from 'react'
 import Typography from '@mui/material/Typography'
 
 const pages: Page[] = [
@@ -23,56 +23,34 @@ type Props = {
 }
 
 const Home: NextPage = () => {
-  const [response, setResponse] = useState(
-    {
-      "data": [
+  const [apiResponse, setApiResponse] = useState<ApiResponse>({})
+
+  useEffect(() => {
+    window.fbAsyncInit = function() {
+      window.FB.init({ 
+        appId: '449280767063215',
+        autoLogAppEvents : true,
+        status: true,
+        version: 'v14.0'
+      });
+
+      FB.api(
+        '/618676662914902/feed',
+        'get',
         {
-          "updated_time": "2022-07-30T11:33:43+0000",
-          "message": "Check out my bedroom",
-          "id": "618676662914902_618714416244460"
+          access_token : 'EAAGYnlSGdK8BADTTeSiTckUaAm5a2QifF6pBsgpZAidSGke9wlbECyBZCLZAuotieNjDEZCMpx5RHkBRqWzb09XIHMZAMLGHgSW8ZC8ZAuZC38IXdFeVLruVjyWk3XFRRUPoSAkvGZCCLFcs8iMrxzZCsbTDU0zCEhHPPWE0BBenqtYwZDZD'
         },
-        {
-          "updated_time": "2022-07-30T11:26:42+0000",
-          "message": "Test post",
-          "id": "618676662914902_618711189578116"
-        },
-        {
-          "updated_time": "2022-07-30T09:58:24+0000",
-          "story": "Natasha Ann created the group Pet-friendly Rentals.",
-          "id": "618676662914902_618676672914901"
+        function(response: ApiResponse) {
+          if (!response || response.error) {
+            console.log(!response ? 'error occured' : response.error)
+          } else {
+            setApiResponse(response)
+            console.log(response)
+          }
         }
-      ],
-      "paging": {
-        "previous": "https://graph.facebook.com/v14.0/618676662914902/feed?access_token=EAAGYnlSGdK8BAA0wjOBBdJGBGAC8Sl3tMW1SPthNbsjC9zrRZAkhBXir0VVCmGDP6HMOAVRmscuoIZC8cigFCTrK6I4oC6W7QesMMfypjGw2Occbd8nB8zGsPCyKMkxZBa5tGysoEQbW6NDHEYUTP6lvPLWojnrrXVtSeC268QnVvCHaoFIW2ESegDhL5sGZBf9P5BgXMxPHR66OGOqJwOX7BNtFdptXgUT45yatFulqkmaOzCSX&pretty=0&__previous=1&since=1659180823&until&__paging_token=enc_AdDzDkAb8sanYUJM2MQzt7zyZCeNmkp2nkBj3sG6GTC2G95ZBrfelL7ryUR7RX8ml7Adn3z3FwNZBJTUTeOuakY2wOgEZBAcAFej0RLcCEUzEc04DdHyMqOChrp7UWeUJUEK2DMSHqSh24ZCdcq1ELFoyBBee",
-        "next": "https://graph.facebook.com/v14.0/618676662914902/feed?access_token=EAAGYnlSGdK8BAA0wjOBBdJGBGAC8Sl3tMW1SPthNbsjC9zrRZAkhBXir0VVCmGDP6HMOAVRmscuoIZC8cigFCTrK6I4oC6W7QesMMfypjGw2Occbd8nB8zGsPCyKMkxZBa5tGysoEQbW6NDHEYUTP6lvPLWojnrrXVtSeC268QnVvCHaoFIW2ESegDhL5sGZBf9P5BgXMxPHR66OGOqJwOX7BNtFdptXgUT45yatFulqkmaOzCSX&pretty=0&until=1659175104&since&__paging_token=enc_AdDFZCOfpTFOx8LvTfYmT67apR3iZC6TXnoNgS1taIL6lEylZCZBYx2HRlDItXJOxvzzAaUeOR7pqjGAKT3EmL1k5z8VUPRuoNlw8OsdPiHDP12kkdfolBviEqYECg9B91isWEjA0ICjADKw96ro498qXfm2&__previous"
-      }
+        );
     }
-  )
-
-  // useEffect(() => {
-  //   window.fbAsyncInit = function() {
-  //     window.FB.init({ 
-  //       appId: '449280767063215',
-  //       autoLogAppEvents : true,
-  //       xfbml            : true,
-  //       version: 'v14.0'
-  //     });
-
-  //     FB.api(
-  //       '/618676662914902/feed',
-  //       'get',
-  //       {},
-  //       function(response: ApiResponse) {
-  //         if (response.data) {
-  //           setResponse(response)
-  //           console.log(response)
-  //         } else {
-  //           console.log(response)
-  //         }
-  //       }
-  //       );
-  //   }
-  // }, [])
+  }, [])
 
   return (
     <Box>
@@ -87,7 +65,7 @@ const Home: NextPage = () => {
           p: "20px"
         }}
       >
-      {response ? response.data.map( (listing) => (
+      {apiResponse.data ? apiResponse.data.map( (listing: Post) => (
         <Grid item xs={6} md={4} lg={2} key={listing.id}>
           <ListingCard listing={listing} />
         </Grid>
@@ -98,12 +76,5 @@ const Home: NextPage = () => {
     </Box>
   )
 }
-
-// export async function getStaticProps() {
-//   const accessToken = 'EAAGYnlSGdK8BANPsPDmmZAXZAhSEyODsWSFE25Pz7zpY7WjPPO5KC18IDUAIZBpDDQEXSluAZCZAAXCqMkaAIhu25yp2jSFfZBIFL1ZBwScXVeoYZAGPOfV0YFTs0DjxgJrNdmxApNvOOhF105hNaWqkxd01v9gDPZCZCeaSZAt1mBedmpxxkW7zi7oWXPYVXD3qUQkc31RJuAeieD6ZBZCvUvQx1CyyZB80Gc6DsohI2GdB1KFwTHLFXiyhlh'
-//   const res = await fetch( "https://graph.facebook.com/v14.0/618676662914902/feed?access_token=" + accessToken)
-//   const response = await res.json()
-//   return { props: { response }}  
-// }
 
 export default Home
--- types/types.tsx
+++ types/types.tsx
@@ -1,21 +1,38 @@
+export type ApiResponse = {
+    data?: Post[];
+    paging?: Paging;
+    error?: ErrorResponse;
+}
+
+// https://developers.facebook.com/docs/graph-api/guides/error-handling/
+export type ErrorResponse = {
+    code: number;
+    error_subcode?: number;
+    error_user_title?: string;
+    error_user_msg?: string;
+    fbtrace_id: string;
+    message: string;
+    type: string;
+}
+
 export type Page = {
     title: string;
     link: string
 }
-
-export type ApiResponse = undefined | {
-    data: Listing[];
-    paging: Paging;
-}
   
 export type Paging = {
-previous: string;
-next: string
+    cursors?: {
+        after?: string;
+        before?: string;
+    };
+    limit?: number;
+    previous: string;
+    next: string;
 }
 
-export type Listing = {
-updated_time: string;
-message?: string;
-story?: string;
-id: string;
+export type Post = {
+    updated_time: string;
+    message?: string;
+    story?: string;
+    id: string;
 }
